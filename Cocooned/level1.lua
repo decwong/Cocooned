fe96905864f44d98a0194939212e7216a06c4f72
Derrick Wong <decwong@ucsc.edu>
2013-11-15 15:19:27 -0800

    Added several changes:
    - Fixed ball pixels, caused bad collision detection
    - Changed images to look like a room.
    - Rewrote most of standard code into an array [table] to make iteration
    simpler.
    - Added a second ball into the game.
    [Note: both balls move at the same time due to the moveBall function]

--- Cocooned/level1.lua
+++ Cocooned/level1.lua
@@ -29,92 +29,107 @@ function scene:createScene( event )
 	local group = self.view
 
 	-- create a grey rectangle as the backdrop
-	local background = display.newImageRect( "background.jpg", display.contentWidth, display.contentHeight )
+	-- temp wood background from http://wallpaperstock.net/wood-floor-wallpapers_w6855.html
+	local background = display.newImageRect( "background2.jpg", screenW+100, screenH)
 	background:setReferencePoint( display.TopLeftReferencePoint )
-	background.x, background.y = 0, 0
-	
+	background.x, background.y = -50, 0
 	
 	-- make a crate (off-screen), position it, and rotate slightly
-	local ball = display.newImage("ball.png")
-	ball.x = 260
-	ball.y = 180
+	local ballTable = { 
+		[1] = display.newImage("ball.png"), 
+		[2] = display.newImage("ball.png") }
+	
+	ballTable[1].x = 260
+	ballTable[1].y = 180
+	ballTable[2].x = 160
+	ballTable[2].y = 180
+	
 	-- add physics to the crate
-	physics.addBody(ball)
+	physics.addBody(ballTable[1])
+	physics.addBody(ballTable[2])
 	
-	local floor1 = display.newImage("ground1.png");
-	floor1.x = -40
-	floor1.y = 180
-	floor1.rotation = 90
+	-- add new walls
+	-- temp wall image from: http://protextura.com/wood-plank-cartoon-11130
 	
-	local floor2 = display.newImage("ground1.png");
-	floor2.x = 520
-	floor2.y = 180
-	floor2.rotation = 90
+	local walls = {
+		[1] = display.newImage("ground1.png"),
+		[2] = display.newImage("ground1.png"),
+		[3] = display.newImage("ground2.png"),
+		[4] = display.newImage("ground2.png") }
 	
-	local floor3 = display.newImage("ground2.png");
-	floor3.x = 30
-	floor3.y = -30
+	walls[1].x = -40
+	walls[1].y = 180
+	walls[1].rotation = 90
 	
-	local floor4 = display.newImage("ground2.png");
-	floor4.x = 30
-	floor4.y = 350
+	walls[2].x = 520
+	walls[2].y = 180
+	walls[2].rotation = 90
 	
-	physics.addBody(floor1, "static", { bounce = 0.01 } )
-	physics.addBody(floor2, "static", { bounce = 0.01 } )
-	physics.addBody(floor3, "static", { bounce = 0.01 } )
-	physics.addBody(floor4, "static", { bounce = 0.01 } )
+	walls[3].x = 250
+	walls[3].y = 5
 	
+	walls[4].x = 250
+	walls[4].y = 315
+	
+	-- apply physics to wall
+	for count = 1, 4, 1 do
+		physics.addBody(walls[count], "static", { bounce = 0.01 } )
+	end
+		
+	-- ball movement control
 	function moveBall(event)
 		local x 
 		local y
 		
-		if event.phase == "ended" then
-			x = event.x - ball.x;
-			y = event.y - ball.y;
-			print (x, y)
+		for count = 1, 2, 1 do
+			if event.phase == "ended" then
+				x = event.x - ballTable[count].x;
+				y = event.y - ballTable[count].y;
+				print (x, y)
 
-			if x < 0 then
-				if x > -30 then
-					if y > 0 then
-						ball:applyLinearImpulse(0,-0.05, ball.x, ball.y)
+				if x < 0 then
+					if x > -30 then
+						if y > 0 then
+							ballTable[count]:applyLinearImpulse(0,-0.05, ballTable[count].x, ballTable[count].y)
+						elseif y < 0 then
+							ballTable[count]:applyLinearImpulse(0,0.05, ballTable[count].x, ballTable[count].y)
+						end
+					elseif y >0 then
+						if y < 30 then
+							ballTable[count]:applyLinearImpulse(0.05, 0, ballTable[count].x, ballTable[count].y)
+						else
+							ballTable[count]:applyLinearImpulse( 0.05, -0.05 ,ballTable[count].x, ballTable[count].y)
+						end
 					elseif y < 0 then
-						ball:applyLinearImpulse(0,0.05, ball.x, ball.y)
+						if y > -30 then
+							ballTable[count]:applyLinearImpulse(0.05, 0, ballTable[count].x, ballTable[count].y)
+						else
+							ballTable[count]:applyLinearImpulse( 0.05, 0.05, ballTable[count].x, ballTable[count].y)
+						end
 					end
-				elseif y >0 then
-					if y < 30 then
-						ball:applyLinearImpulse(0.05, 0, ball.x, ball.y)
-					else
-						ball:applyLinearImpulse( 0.05, -0.05 ,ball.x, ball.y)
-					end
-				elseif y < 0 then
-					if y > -30 then
-						ball:applyLinearImpulse(0.05, 0, ball.x, ball.y)
-					else
-						ball:applyLinearImpulse( 0.05, 0.05, ball.x, ball.y)
+				elseif x > 0 then
+					if x < 30 then
+						if y > 0 then
+							ballTable[count]:applyLinearImpulse(0,-0.05, ballTable[count].x, ballTable[count].y)
+						elseif y < 0 then
+							ballTable[count]:applyLinearImpulse(0,0.05, ballTable[count].x, ballTable[count].y)
+						end
+					elseif y < 0 then
+						if y > -30 then
+							ballTable[count]:applyLinearImpulse(-0.05, 0, ballTable[count].x, ballTable[count].y)
+						else
+							ballTable[count]:applyLinearImpulse( -0.05, 0.05, ballTable[count].x, ballTable[count].y)
+						end
+					elseif y > 0 then
+						if y < 30 then
+							ballTable[count]:applyLinearImpulse(-0.05, 0, ballTable[count].x, ballTable[count].y)
+						else
+							ballTable[count]:applyLinearImpulse( -0.05, -0.05, ballTable[count].x, ballTable[count].y)
+						end
 					end
 				end
-			elseif x > 0 then
-				if x < 30 then
-					if y > 0 then
-						ball:applyLinearImpulse(0,-0.05, ball.x, ball.y)
-					elseif y < 0 then
-						ball:applyLinearImpulse(0,0.05, ball.x, ball.y)
-					end
-				elseif y < 0 then
-					if y > -30 then
-						ball:applyLinearImpulse(-0.05, 0, ball.x, ball.y)
-					else
-						ball:applyLinearImpulse( -0.05, 0.05, ball.x, ball.y)
-					end
-				elseif y > 0 then
-					if y < 30 then
-						ball:applyLinearImpulse(-0.05, 0, ball.x, ball.y)
-					else
-						ball:applyLinearImpulse( -0.05, -0.05, ball.x, ball.y)
-					end
-				end
+				
 			end
-			
 		end
 	end
 	
@@ -124,7 +139,8 @@ function scene:createScene( event )
 	
 	-- all display objects must be inserted into group
 	group:insert( background )
-	group:insert( ball )
+	group:insert( ballTable[1] )
+	group:insert( ballTable[2] )
 end
 
 -- Called immediately after scene has moved onscreen:
